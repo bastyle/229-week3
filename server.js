dfde9f96d1615cbcda2373e38e4b9dd193205e89
Bastian <bastian.bastias@gmail.com>
2022-09-20 21:24:06 -0400

    form params

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const exp = require('express');
 const app = exp();
 const path = require("path")
 const bodyParser = require('body-parser');
+const { nextTick } = require('process');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname,"home.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.use("/about_us", function(req, res){//doesn care the http method that are using
-    res.sendFile(path.join(__dirname,"about_us.html"));
+app.use("/about_us", function (req, res) {//doesn care the http method that are using
+    res.sendFile(path.join(__dirname, "about_us.html"));
 });
 
 // params past & callback function
@@ -24,18 +25,25 @@ app.post("/login", function(req, pos){
 })*/
 
 app.route("/login")
-    .get(function(req, res){
-        res.sendFile(path.join(__dirname,"login.html"));
+    .get(function (req, res) {
+        res.sendFile(path.join(__dirname, "login.html"));
     })
-    .post(function(req, res){
-        console.log("req: "+(req.body))
-        console.log("username:" +req.body.username);
-        console.log("password:" +req.body.password);
-        res.sendFile(path.join(__dirname,"loged.html"));
+    .post(function (req, res) {
+        console.log("req: " + (req.body))
+        console.log("username:" + req.body.username);
+        console.log("password:" + req.body.password);
+        res.sendFile(path.join(__dirname, "loged.html"));
     });
 
+//
+app.get("/product/:id", function (req, res) {
+    var product_id=req.params.id;
+    console.log("product_id: "+product_id);
+    res.send("product_id: "+product_id);
+})
+
 var port = process.env.PORT || 8080; //default setting or value for port var
-console.log("port: "+process.env.PORT)
+console.log("port: " + process.env.PORT)
 app.listen(port);
 
 
